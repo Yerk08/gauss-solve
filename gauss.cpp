dfbf4e037b0d8deb9f6f43fc3fb73c022645d67a
yerk <eremejperv@gmail.com>
2025-09-12 09:30:04 +0300

    better

--- gauss.cpp
+++ gauss.cpp
@@ -31,8 +31,9 @@ int gcd(int a, int b) {
 }
 
 class MyInt : public OpNot<MyInt> {
-public:
+private:
 	int n, m;
+public:
 	MyInt() {}
 	MyInt(int i) : n(i), m(1) {}
 	MyInt(int i, int j) {
@@ -67,91 +68,100 @@ public:
 	bool operator==(const int other) const {
 		return (n == other && m == 1);
 	}
+
+	friend istream& operator>>(istream& cin, MyInt& a) {
+		a.m = 1;
+		return cin >> a.n;
+	}
+
+	friend ostream& operator<<(ostream& cout, const MyInt a) {
+		if (a.m == 1) {
+			return cout << a.n;
+		}
+		return cout << a.n << "/" << a.m;
+	}
 };
 
-istream& operator>>(istream& cin, MyInt& a) {
-	a.m = 1;
-	return cin >> a.n;
-}
 
-istream& operator>>(istream& cin, vector <vector <MyInt>>& a) {
-	for (size_t i = 0; i < a.size(); ++i) {
-		for (size_t j = 0; j < a[i].size(); ++j) {
-			cin >> a[i][j];
-		}
-	}
-	return cin;
-}
-
-ostream& operator<<(ostream& cout, MyInt a) {
-	if (a.m == 1) {
-		return cout << a.n;
-	}
-	return cout << a.n << "/" << a.m;
-}
-
-ostream& operator<<(ostream& cout, vector <vector <MyInt>>& a) {
-	for (size_t i = 0; i < a.size(); ++i) {
-		for (size_t j = 0; j < a[i].size(); ++j) {
-			cout << a[i][j] << "\t";
-		}
-		cout << "\n";
-	}
-	return cout;
-}
-
-void first_operator(vector <vector <MyInt>>& mt, const size_t i, const size_t j) {
-	swap(mt[i], mt[j]);
-	cout << "-- swap (" << i + 1 << ") and (" << j + 1 << ") rows\n" << mt << "\n";
-}
-
-void second_operator(vector <vector <MyInt>>& mt, const size_t i, const MyInt a) {
-	for (int j = 0; j < mt[i].size(); ++j) {
-		mt[i][j] = mt[i][j] * a;
-	}
-	cout << "-- multiply row (" << i + 1 << ") by " << a << "\n" << mt << "\n";
-}
-
-void third_operator(vector <vector <MyInt>>& mt, const size_t i, const size_t j, const MyInt a) {
-	if (i == j) throw runtime_error("Add two same rows");
-	for (int k = 0; k < mt[i].size(); ++k) {
-		mt[i][k] = mt[i][k] + mt[j][k] * a;
-	}
-	cout << "-- Add to row (" << i + 1 << ") row (" << j + 1 << ") mul. by " << a << "\n" << mt << "\n";
-}
-
-void gauss_method(vector <vector <MyInt>>& mt) {
-	size_t cur_i = 0, cur_j = 0, cur_iterate = 0;
-	while (cur_i < mt.size() && cur_j < mt.back().size()) {
-		cout << "  <ITERATE " << (++cur_iterate) << ">  \n";
-		if (mt[cur_i][cur_j] != 0) { // part 1
-			for (size_t k = 0; k < mt.size(); ++k) {
-				if (k != cur_i) {
-					MyInt a = -mt[k][cur_j] / mt[cur_i][cur_j];
-					if (a != 0) third_operator(mt, k, cur_i, a);
-				}
+template <typename T>
+class matrix {
+private:
+	vector <vector <T>> mt;
+public:
+	matrix() {}
+	matrix(int n, int m) : mt(vector <vector <T>>(n, vector <T>(m, 0))) {}
+	
+	friend istream& operator>>(istream& cin, matrix<T>& a) {
+		for (size_t i = 0; i < a.mt.size(); ++i) {
+			for (size_t j = 0; j < a.mt[i].size(); ++j) {
+				cin >> a.mt[i][j];
 			}
-			MyInt a = MyInt(1) / mt[cur_i][cur_j];
-			if (a != 1) second_operator(mt, cur_i, a);
-			++cur_i;
-			++cur_j;
-		} else { // part 2
-			bool found = false;
-			for (size_t k = cur_i; k < mt.size(); ++k) {
-				if (mt[k][cur_j] != 0) {
-					found = true;
-					first_operator(mt, cur_i, k);
-					break;
-				}
+		}
+		return cin;
+	}
+	
+	friend ostream& operator<<(ostream& cout, const matrix<T>& a) {
+		for (size_t i = 0; i < a.mt.size(); ++i) {
+			for (size_t j = 0; j < a.mt[i].size(); ++j) {
+				cout << a.mt[i][j] << "\t";
 			}
-			if (!found) { // part 3
+			cout << "\n";
+		}
+		return cout;
+	}
+
+	void first_operator(const size_t i, const size_t j) {
+		swap(mt[i], mt[j]);
+		cout << "-- swap (" << i + 1 << ") and (" << j + 1 << ") rows\n" << *this << "\n";
+	}
+
+	void second_operator(const size_t i, const T a) {
+		for (int j = 0; j < mt[i].size(); ++j) {
+			mt[i][j] = mt[i][j] * a;
+		}
+		cout << "-- multiply row (" << i + 1 << ") by " << a << "\n" << *this << "\n";
+	}
+
+	void third_operator(const size_t i, const size_t j, const T a) {
+		if (i == j) throw runtime_error("Add two same rows");
+		for (int k = 0; k < mt[i].size(); ++k) {
+			mt[i][k] = mt[i][k] + mt[j][k] * a;
+		}
+		cout << "-- Add to row (" << i + 1 << ") row (" << j + 1 << ") mul. by " << a << "\n" << *this << "\n";
+	}
+
+	void gauss_method() {
+		size_t cur_i = 0, cur_j = 0, cur_iterate = 0;
+		while (cur_i < mt.size() && cur_j < mt.back().size()) {
+			cout << "  <ITERATE " << (++cur_iterate) << ">  \n";
+			if (mt[cur_i][cur_j] != 0) { // part 1
+				for (size_t k = 0; k < mt.size(); ++k) {
+					if (k != cur_i) {
+						T a = -mt[k][cur_j] / mt[cur_i][cur_j];
+						if (a != 0) third_operator(k, cur_i, a);
+					}
+				}
+				T a = T(1) / mt[cur_i][cur_j];
+				if (a != 1) second_operator(cur_i, a);
+				++cur_i;
 				++cur_j;
+			} else { // part 2
+				bool found = false;
+				for (size_t k = cur_i; k < mt.size(); ++k) {
+					if (mt[k][cur_j] != 0) {
+						found = true;
+						first_operator(cur_i, k);
+						break;
+					}
+				}
+				if (!found) { // part 3
+					++cur_j;
+				}
 			}
 		}
+		cout << "  <FINISHED>  \n";
 	}
-	cout << "  <FINISHED>  \n";
-}
-
+};
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -159,8 +169,8 @@ int main() {
 	cout.tie(0);
 	int n, m;
 	cin >> n >> m;
-	auto mt = vector <vector <MyInt>>(n, vector <MyInt>(m, 0));
+	matrix<MyInt> mt(n, m);
 	cin >> mt;
-	gauss_method(mt);
+	mt.gauss_method();
 }
 
